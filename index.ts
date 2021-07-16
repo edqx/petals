1ac34c70b4d1865b92b2f39b805e95ff3c511b25
Edward Smale <essmale2005@gmail.com>
2021-07-16 02:01:16 +0100

    Update exports

--- index.ts
+++ index.ts
@@ -5,5 +5,6 @@ export * as Data from "./lib/blocks/data";
 export * as Looks from "./lib/blocks/looks";
 export * as Motion from "./lib/blocks/motion";
 export * as Operators from "./lib/blocks/operators";
+export * as Procedure from "./lib/blocks/procedure";
 
 export * from "./lib/blocks";
