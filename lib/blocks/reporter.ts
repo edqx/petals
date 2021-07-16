2c014bde84ca22c7e66cffff24b501b7f2e4d1ec
Edward Smale <essmale2005@gmail.com>
2021-07-16 02:01:12 +0100

    Update exports

--- lib/blocks/reporter.ts
+++ lib/blocks/reporter.ts
@@ -3,6 +3,9 @@ import { Opcode } from "../types";
 import * as Control from "./control";
 import * as Data from "./data";
 import * as Looks from "./looks";
+import * as Motion from "./motion";
+import * as Operators from "./operators";
+import * as Procedure from "./procedure";
 
 export abstract class Reporter extends Block {
   static getOpcodes(): { [opcode: Opcode]: Reporter } {
@@ -36,69 +39,69 @@ export abstract class Reporter extends Block {
        * Motion reporter opcodes: {@link "https://github.com/LLK/scratch-vm/blob/develop/src/blocks/scratch3_motion.js"}
        */
 
-      motion_xposition:
-      motion_yposition:
-      motion_direction:
-      motion_xscroll:
-      motion_yscroll:
+      motion_xposition: Motion.XPosition,
+      motion_yposition: Motion.YPosition,
+      motion_direction: Motion.Direction,
+      motion_xscroll: Motion.XScroll,
+      motion_yscroll: Motion.YScroll,
 
       /**
        * Operator reporter opcodes: {@link https://github.com/LLK/scratch-vm/blob/develop/src/blocks/scratch3_operators.js}
        */
 
-      operator_add: 
-      operator_subtract: 
-      operator_multiply: 
-      operator_divide: 
-      operator_lt: 
-      operator_equals: 
-      operator_gt: 
-      operator_and: 
-      operator_or: 
-      operator_not: 
-      operator_random: 
-      operator_join: 
-      operator_letter_of: 
-      operator_length: 
-      operator_contains: 
-      operator_mod: 
-      operator_round: 
-      operator_mathop: 
+      operator_add: Operators.Add,
+      operator_subtract: Operators.Subtract,
+      operator_multiply: Operators.Multiply,
+      operator_divide: Operators.Divide,
+      operator_lt: Operators.LessThan,
+      operator_equals: Operators.Equals,
+      operator_gt: Operators.GreaterThan,
+      operator_and: Operators.And,
+      operator_or: Operators.Or,
+      operator_not: Operators.Not,
+      operator_random: Operators.Random,
+      operator_join: Operators.Join,
+      operator_letter_of: Operators.LetterOf,
+      operator_length: Operators.Length,
+      operator_contains: Operators.Contains,
+      operator_mod: Operators.Modulo,
+      operator_round: Operators.Round,
+      operator_mathop: Operators.MathOperation,
 
       /**
        * Procedures reporter opcodes: {@link "https://github.com/LLK/scratch-vm/blob/develop/src/blocks/scratch3_procedures.js"}
        */
 
-      argument_reporter_string_number:
-      argument_reporter_boolean:
+      argument_reporter_string_number: Procedure.ProcedureStringNumberArgument,
+      argument_reporter_boolean: Procedure.ProcedureBooleanArgument,
 
       /**
        * Sensing reporter opcodes: {@link "https://github.com/LLK/scratch-vm/blob/develop/src/blocks/scratch3_sensing.js"}
        */
 
-      sensing_touchingobject: 
-      sensing_touchingcolor: 
-      sensing_coloristouchingcolor: 
-      sensing_distanceto: 
-      sensing_timer: 
-      sensing_of: 
-      sensing_mousex: 
-      sensing_mousey: 
-      sensing_mousedown: 
-      sensing_keypressed: 
-      sensing_current: 
-      sensing_dayssince2000: 
-      sensing_loudness: 
-      sensing_loud: 
-      sensing_answer: 
-      sensing_username: 
-      sensing_userid: 
+      sensing_touchingobject:
+      sensing_touchingcolor:
+      sensing_coloristouchingcolor:
+      sensing_distanceto:
+      sensing_timer:
+      sensing_of:
+      sensing_mousex:
+      sensing_mousey:
+      sensing_mousedown:
+      sensing_keypressed:
+      sensing_current:
+      sensing_dayssince2000:
+      sensing_loudness:
+      sensing_loud:
+      sensing_answer:
+      sensing_username:
+      sensing_userid:
 
       /**
        * Sound reporter opcodes: {@link "https://github.com/LLK/scratch-vm/blob/develop/src/blocks/scratch3_sound.js"}
        */
 
-      sound_volume: 
+      sound_volume:
     }
   }
 }
