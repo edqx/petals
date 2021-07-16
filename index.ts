4db5ca5b85f36032d663b7d6ff298eac865b78d5
Edward Smale <essmale2005@gmail.com>
2021-07-16 02:00:54 +0100

    Update exports

--- index.ts
+++ index.ts
@@ -1,3 +1,9 @@
 export { PetalsValue as Value } from "./lib/types";
 
 export * as Control from "./lib/blocks/control";
+export * as Data from "./lib/blocks/data";
+export * as Looks from "./lib/blocks/looks";
+export * as Motion from "./lib/blocks/motion";
+export * as Operators from "./lib/blocks/operators";
+
+export * from "./lib/blocks";
